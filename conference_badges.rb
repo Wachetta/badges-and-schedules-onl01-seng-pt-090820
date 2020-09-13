14184a11bf8e88a94455f55449679cd62f783c9c
Zachary Darneille <zakdarn@gmail.com>
2020-09-13 04:09:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,11 +16,11 @@ def assign_rooms(attendees)
   end
 
   def printer(attendees)
-    batch_badge_creator(attendees).each do |thing|
-      puts thing
-    end
-
-    assign_rooms(attendees).each do |room|
-      puts room
-    end
+  batch_badge_creator(attendees).each do |badges|
+    puts badges
   end
+
+  assign_rooms(attendees).each do |room|
+    puts room
+  end
+end
